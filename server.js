19d3dfc039aa3b0945386f603e05c9e17475413b
Hwang gend <ghkdtjsdks@naver.com>
2025-12-04 21:15:49 +0900

    feat: cors 설정 추가

--- server.js
+++ server.js
@@ -12,6 +12,16 @@ const problemRoutes = require("./routes/problem_mysql");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
 // 미들웨어
 app.use(cors());
